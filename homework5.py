c9b501df02df92b359333e7dce25af2e20d624fd
Yanysik111 <yanyisik@yandex.ru>
2024-05-19 14:15:28 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['banana', 'pineapple', 'coconut', 'melon', 'apple']
+print('Список:', my_list)
+print('Первый элемент:', my_list[0])
+print('Последний элемент:', my_list[4])
+print('Подсписок с 3 по 5 элементы:', my_list[2:5])
+my_list[2] = 'orange'
+print('Новый список:', my_list)
+my_dict = {'sun': 'солнце', 'dog': 'собака', 'cat': 'кот', 'child': 'ребенок'}
+print('Словарь:',my_dict)
+print('Перевод слова:',my_dict['cat'])
+my_dict['cat'] = 'кошка'
+print('Новый словарь:',my_dict)
